51d9e7f5f1e67aab58b9b0adacae1dad6e28bc8b
Debapriya Majumder <guddumajumder75@gmail.com>
2021-07-09 11:02:24 +0530

    added decision tree

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,13 +28,36 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
-x = len(features_train)
-y = len(labels_train)
+# x = len(features_train)
+# y = len(labels_train)
 
-features_train = features_train[:(x//100)]
-labels_train = labels_train[:(y//100)]
+# features_train = features_train[:(x//100)]
+# labels_train = labels_train[:(y//100)]
 
-clf = SVC(kernel="linear")
+# ls= [10.0,100.0,1000.0,10000.0]
+# # ls= [1.0]
+
+# for c in ls:
+#     clf = SVC(kernel="rbf",C=c, gamma= "auto")
+
+#     t0 = time()
+
+#     clf.fit(features_train,labels_train)
+
+#     print("training time: ", round(time()-t0,3) , "s" )
+
+#     t0 = time()
+
+#     pred=clf.predict(features_test)
+
+#     print("prediction time: ", round(time()-t0,3) , "s" )
+
+
+#     score = accuracy_score(labels_test,pred)
+
+#     print("C =",c,",score =",score);
+
+clf = SVC(kernel="rbf",C=10000.0, gamma= "auto")
 
 t0 = time()
 
@@ -48,10 +71,18 @@ pred=clf.predict(features_test)
 
 print("prediction time: ", round(time()-t0,3) , "s" )
 
+# print(pred[10],pred[26],pred[50])
+cnt=0
+for i in pred:
+    if i:
+        cnt+=1
 
-score = accuracy_score(labels_test,pred)
+print(cnt)
 
-print(score)
+# score = accuracy_score(labels_test,pred)
+
+
+# print(score)
 
 #########################################################
 
