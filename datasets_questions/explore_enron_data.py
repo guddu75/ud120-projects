a508122c51b2118d6cad6deb43e258106a5017a4
Debapriya Majumder <guddumajumder75@gmail.com>
2021-07-14 09:21:19 +0530

    python 3 changes

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,24 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+# print(type(enron_da))
+# print(enron_data.keys())
+# print(len(enron_data[list(enron_data.keys())[0]]))
+cnt=0
+cnt1=0
+for person in enron_data.keys():
+    if enron_data[person]['poi']:
+        cnt1+=1
+        if enron_data[person]['total_payments']=="NaN":
+            cnt+=1
+    
+
+print(cnt1,cnt)
+
+# a= enron_data["SKILLING JEFFREY K"]['total_payments']
+# b= enron_data["LAY KENNETH L"]['total_payments']
+# c= enron_data["FASTOW ANDREW S"]['total_payments']
+
+# print(max(a,b,c))
+
 
