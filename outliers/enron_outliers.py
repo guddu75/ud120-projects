056ac85427b7c1521ff16f65bb05b22a0cc06a58
Debapriya Majumder <guddumajumder75@gmail.com>
2021-07-15 15:30:11 +0530

    changed outlier

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -8,12 +8,43 @@ from feature_format import featureFormat, targetFeatureSplit
 
 
 ### read in data dictionary, convert to numpy array
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
+# print(type(data_dict))
+
+# print(data_dict.keys())
+# lst=[]
+# # print(data_dict[list(data_dict.keys())[0]])
+# k = list(data_dict.keys())
+# print(k[103])
+# print(type(data_dict[k[0]]['salary']))
+# for i in range(len(k)):
+#     # temp= (data_dict[k[i]]['salary'],int(i))
+#     if data_dict[k[i]]['salary']=='NaN':
+#         temp = (0,i);
+#     else:
+#         temp= (data_dict[k[i]]['salary'],i)
+#     lst.append(temp)
+#     # print(temp)
+
+# lst.sort()
+# print(lst)
+
+data_dict.pop('TOTAL',0)
+
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,10 +10,29 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
+    a=[]
+    b=[]
+    c=[]
+    for i in range(len(ages)):
+        a.append((abs(predictions[i][0]-net_worths[i][0]))**2)
+        b.append(ages[i][0])
+        c.append(net_worths[i][0])
+    # print(a)
+    # print(b)
+    # print(c)
+    lst=[]
+    for i in range(len(ages)):
+        lst.append((a[i],b[i],c[i]));
+    
+    lst.sort()
+    for a,b,c in lst:
+        print(a,b,c)
     
     cleaned_data = []
-
+    for error , age , nw in lst:
+        cleaned_data.append((age,nw,error))
     ### your code goes here
+    cleaned_data = cleaned_data[:81]
 
     
     return cleaned_data
